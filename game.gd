4386ccf5100fb842dce761defbe49bdd9b2216e8
Victor <victormeunier.dev@gmail.com>
2020-02-29 00:00:28 +0100

    ftt from mic working

--- game.gd
+++ game.gd
@@ -1,29 +1,28 @@
 extends Node2D
 
-const VU_COUNT = 16
+const VU_COUNT = 360
 const FREQ_MAX = 11050.0
 
-const WIDTH = 400
+onready var width = get_viewport().size.x
 const HEIGHT = 100
 
 const MIN_DB = 60
 
 var spectrum = null
 var recording
-var effect
+var effect = null
 
 func _draw():
-	if spectrum != null and spectrum.is_recording_active():
+	if effect != null and effect.is_recording_active():
 		#warning-ignore:integer_division
-		var w = WIDTH / VU_COUNT
+		var w = width / VU_COUNT
 		var prev_hz = 0
 		for i in range(1, VU_COUNT+1):	
 			var hz = i * FREQ_MAX / VU_COUNT;
-			var magnitude: float = effect.get_magnitude_for_frequency_range(prev_hz, hz).length()
+			var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
 			var energy = clamp((MIN_DB + linear2db(magnitude)) / MIN_DB, 0, 1)
 			var height = energy * HEIGHT
 			draw_rect(Rect2(w * i, HEIGHT - height, w, height), Color.white)
-			print(height)
 			prev_hz = hz
 
 
@@ -32,9 +31,13 @@ func _process(_delta):
 
 
 func _ready():
-	spectrum = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"),0)
-	spectrum.set_recording_active()
-	recording = spectrum.get_recording()
-	$AudioStreamRecord.stream = recording
-	$AudioStreamRecord.play()
-	effect = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("Record"), 0)
+	effect = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
+	effect.set_recording_active(true)
+	
+	get_tree().root.connect("size_changed", self, "_on_viewport_size_changed")
+	spectrum = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("Record"), 1)
+
+func _on_viewport_size_changed():
+	# Do whatever you need to do when the window changes!
+	print ("Viewport size changed")
+	width = get_viewport().size.x
