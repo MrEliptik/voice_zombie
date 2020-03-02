5b1fce1f25d5b1a5777c8ef690167630a12ec56c
Victor <victormeunier.dev@gmail.com>
2020-03-02 20:01:35 +0100

    update

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _draw():
 	pass
@@ -29,3 +29,6 @@ func _draw():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	update()
+
+func _on_Area2D_body_entered(body):
+	print("DEAD")
