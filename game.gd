282f4ab5c8e4c9e4bca489e717170be5a20d050d
Victor MEUNIER <vmeunier@signoptic.local>
2020-03-02 13:43:00 +0100

    reduce vu count and add raycasting (WIP)

--- game.gd
+++ game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const VU_COUNT = 360
+const VU_COUNT = 180
 const FREQ_MAX = 11050.0
 
 const enemy = preload("res://enemy.tscn")
@@ -16,6 +16,7 @@ const BASE_CIRCLE = 60
 var spectrum = null
 var recording
 var effect = null
+var points
 
 onready var color = Color(randf(), randf(), randf())
 
@@ -24,7 +25,7 @@ func _draw():
 		#warning-ignore:integer_division
 		var w = window_w / VU_COUNT
 		var prev_hz = 0
-		var points = PoolVector2Array([])
+		points = PoolVector2Array([])
 		for i in range(1, VU_COUNT+1):	
 			var hz = i * FREQ_MAX / VU_COUNT;
 			var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
@@ -41,13 +42,26 @@ func _draw():
 		draw_colored_polygon(points, color)
 		
 
-	draw_colored_polygon(points, Color(1.0, 1.0, 1.0))
+	#draw_colored_polygon(points, Color(1.0, 1.0, 1.0))
 
 func _process(_delta):
 	update()
+
+func _physics_process(delta):
+	if points:
+		for point in points:
+			# Cast to point
+			$Player/RayCast2D.set_cast_to(Vector2(point.x, point.y))
+			#print(Vector2(point.x, point.y))
+#			# Check if collision
+			$Player/RayCast2D.force_raycast_update()
+			if $Player/RayCast2D.is_colliding():
+				var enemy = $Player/RayCast2D.get_collider()
+				print(enemy)
+				$EnemiesContainer.remove_child(enemy)
 	
 	for i in range($EnemiesContainer.get_child_count()):
-		$EnemiesContainer.get_child(i).move_toward($Player.global_position)
+		$EnemiesContainer.get_child(i).move_toward($Player/RayCast2D.global_position)
 
 
 func _ready():
@@ -59,6 +73,8 @@ func _ready():
 	get_tree().root.connect("size_changed", self, "_on_viewport_size_changed")
 	spectrum = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("Record"), 1)
 	
+	$Player/RayCast2D.position = offset
+	
 	# Spawn enemies
 	for i in range(ENEMIES_NB):
 		$EnemiesContainer.add_child(enemy.instance())
@@ -67,7 +83,9 @@ func _on_viewport_size_changed():
 	# Do whatever you need to do when the window changes!
 	print ("Viewport size changed")
 	window_w = get_viewport().size.x
-	offset = window_w/2
+	window_h = get_viewport().size.y
+	offset = Vector2(window_w/2, window_h/2)
+	$Player/RayCast2D.position = offset
 
 func _on_Timer_timeout():
 	color = Color(randf(), randf(), randf())
