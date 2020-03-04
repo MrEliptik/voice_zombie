aa5d7c911a376dabf1846e4e77fb2d04f485daea
Victor <victormeunier.dev@gmail.com>
2020-03-04 22:25:38 +0100

    clean

--- startScreen.gd
+++ startScreen.gd
@@ -2,20 +2,14 @@ extends Control
 
 signal start
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
 	if !visible: return
-	if event.is_action_released("ui_accept"):
+	if event.is_action_released("ui_accept") \
+		or event.is_action_released("ui_left_click") \
+		or event.is_action_released("ui_right_click"):
 		emit_signal("start")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
